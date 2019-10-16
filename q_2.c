b648fffb2c5401337415085e651dd77e06d3c6db
s00r4j <56623417+s00r4j@users.noreply.github.com>
2019-10-16 11:43:22 +0530

    Create q_2.c

---
+++ q_2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+  int i,j;
+  int s=0;
+  for(i=1;i<=5;i++)
+  {
+  	for(j=5;j>=s;j--)
+	{
+		printf(" ");
+	}
+	for(j=1;j<=i;j++)
+	{
+		printf("%d",j);
+	}
+	for(j=i-1;j>=1;j--)
+	{
+		printf("%d",j);
+	}
+	printf("\n");
+	s++;
+  }
+}
+
